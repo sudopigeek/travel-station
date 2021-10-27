6ea0907e60410e3275d85c76cdbf79e2994f2cb7
sudopigeek <samuelcatferrell@gmail.com>
2021-10-27 12:28:03 -0700

    feat(travelstation): Added simple account info page

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -1,6 +1,33 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 import { getUserByName } from "../../modules/APIManager"
 
 export const Account = () => {
-    
+    const [account, setAccount] = useState({name: "", email: "", password: "", isAdmin: false});
+    const getAccount = userName => {
+        getUserByName(userName).then(data => { 
+            setAccount({
+                name: data[0].name,
+                email: data[0].email,
+                password: data[0].password,
+                isAdmin: data[0].isAdmin
+            }) 
+        });
+    }
+
+    useEffect(() => {
+        getAccount(sessionStorage.getItem("user"))
+    }, [])
+
+    return (
+        <main className="account">
+            {account.name !== "" ? 
+                <section>
+                    <h3>Name: {account.name}</h3>
+                    <p>Email: {account.email}</p>
+                    <p>Password: ********</p>
+                    <p>Account Type: {account.isAdmin ? <strong>Administrator</strong> : <strong>Visitor</strong>}</p>
+                </section> 
+            : null}
+        </main>
+    );
 }
