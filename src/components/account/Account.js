ec20c8586216fe8feab9a83a5e08d607604cdf94
sudopigeek <samuelcatferrell@gmail.com>
2021-11-01 12:58:01 -0700

    feat(travelstation): Removed password fields in app and password data in json

--- src/components/account/Account.js
+++ src/components/account/Account.js
@@ -3,13 +3,12 @@ import { getUserByName } from "../../modules/APIManager"
 import './Account.css';
 
 export const Account = () => {
-    const [account, setAccount] = useState({name: "", email: "", password: "", isAdmin: false});
+    const [account, setAccount] = useState({name: "", email: "", isAdmin: false});
     const getAccount = userName => {
         getUserByName(userName).then(data => { 
             setAccount({
                 name: data[0].name,
                 email: data[0].email,
-                password: data[0].password,
                 isAdmin: data[0].isAdmin
             }) 
         });
@@ -26,7 +25,6 @@ export const Account = () => {
                     <section>
                         <h3>Name: {account.name}</h3>
                         <p>Email: {account.email}</p>
-                        <p>Password: ********</p>
                         <p>Account Type: {account.isAdmin ? <strong>Administrator</strong> : <strong>Visitor</strong>}</p>
                     </section> 
                 : null}
